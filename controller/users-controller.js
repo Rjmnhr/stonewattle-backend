2deda0c5ce31cf58172948fef08ece221d0f6145
rjmnhr1 <renjith.cm@refactor.academy>
2023-09-22 16:33:23 +0530

    update

--- controller/users-controller.js
+++ controller/users-controller.js
@@ -1,13 +1,65 @@
 const Users = require("../models/users-model");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
 function generateAccessToken(userId) {
   return jwt.sign({ id: userId }, process.env.SECRET_KEY, { expiresIn: "5d" });
 }
 
+const notifyByMail = (data) => {
+  const { first_name, last_name, email } = data;
+
+  // Create a Nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "info@2ndstorey.com",
+      pass: "secondstorey",
+    },
+  });
+
+  // Set up email data
+  const mailOptions = {
+    from: "info@2ndstorey.com",
+    to: "Indradeep.mazumdar@gmail.com",
+    subject: `New Customer Registration Notification`,
+    text: `Hello 2nd Storey,
+    
+We are pleased to inform you that a new customer has registered on our platform. Below are the details of the new customer:
+    
+Customer Name: ${first_name}  ${last_name}
+Email Address: ${email}
+   
+    
+This new customer has expressed interest in our services and products.
+
+Thank you for your attention, and let's work together to ensure our new customer has a great experience with our company.
+    
+Best regards,
+
+2nd Storey Team
+  `,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) {
+      console.error("Error sending email:", error);
+    } else {
+      console.log(" send successfully");
+    }
+  });
+};
+
 const UsersController = {
   createGoogleUser: async (req, res) => {
+    console.log(
+      "🚀 ~ file: users-controller.js:60 ~ createGoogleUser: ~ req:",
+      req.body
+    );
     try {
       const existingUser = (await Users.loginUser(req.body))[0];
 
@@ -26,6 +78,8 @@ const UsersController = {
         if (!CreateUserData)
           return res.status(402).json("creating Google user failed");
 
+        notifyByMail(req.body);
+
         const newUser = (await Users.loginUser(req.body))[0];
         const accessToken = generateAccessToken(newUser.id);
 
@@ -40,6 +94,8 @@ const UsersController = {
   createUser: async (req, res) => {
     try {
       const data = await Users.createUser(req.body);
+      notifyByMail(req.body);
+
       res.status(200).json(data);
     } catch (err) {
       console.error(err);
